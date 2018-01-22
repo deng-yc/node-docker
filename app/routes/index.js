240352ff2253c761fb28be752d4cb97015bf830f
dengyc <dengyc2013@gmail.com>
2018-01-22 14:22:44 +0800

    updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  fetch({
+    url:"http://webapi:3000/test"
+  }).then(req=>{
+    return req.json()
+  }).then(json=>{
+    res.render('index', { title: 'Express:'+json.url,now:json.now });
+  })
+
 });
 
 module.exports = router;
