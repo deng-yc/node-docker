e7a4e493ebd065da6c64862e89b5c90a86a1fd72
dengyc <dengyc2013@gmail.com>
2018-01-22 14:51:03 +0800

    updated,fetch

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,9 +6,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  fetch({
-    url:"http://webapi:3000/test"
-  }).then(req=>{
+  fetch("http://webapi:3000/test").then(req=>{
     return req.json()
   }).then(json=>{
     res.render('index', { title: 'Express:'+json.url,now:json.now });
