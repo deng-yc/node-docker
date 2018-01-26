dad0b35c5d48d733729f9cb154be82b51935eff3
dengyc <dengyc2013@gmail.com>
2018-01-26 10:35:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
-#!/bin/bash  
-arr=("app" "api")
+#!/bin/bash 
 
-for var in ${arr[@]};  
-do  
-    echo --------------开始编译 $var----------------
-    docker build -t hicoin-web-$var ./$var
+declare -A dic
+dic=([app]="./app" [api]="./api")
+
+
+for key in $(echo ${!dic[*]})
+do
+    echo "$key : ${dic[$key]}"
+    echo --------------开始编译 $key----------------
+    docker build -t hicoin-web-$key ${dic[$key]}
     echo --------------添加TAG----------------
-    docker tag hicoin-web-$var 192.168.31.245:5000/hicoin-web-$var:v1.${BUILD_NUMBER}
+    docker tag hicoin-web-$key 192.168.31.245:5000/hicoin-web-$key:v1.${BUILD_NUMBER}
     echo --------------推送到仓库----------------
-    docker push 192.168.31.245:5000/hicoin-web-$var:v1.${BUILD_NUMBER}
+    docker push 192.168.31.245:5000/hicoin-web-$key:v1.${BUILD_NUMBER}
     echo --------------删除本地镜像----------------
-    docker rmi hicoin-web-$var
-    docker rmi 192.168.31.245:5000/hicoin-web-$var:v1.${BUILD_NUMBER}
-done  
+    docker rmi hicoin-web-$key
+    docker rmi 192.168.31.245:5000/hicoin-web-$key:v1.${BUILD_NUMBER}
+done
