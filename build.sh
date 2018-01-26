e7e75ae058eba12baed4f3eeffe0b99e94be2410
dengyc <dengyc2013@gmail.com>
2018-01-26 10:31:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ do
     echo --------------开始编译 $var----------------
     docker build -t hicoin-web-$var ./$var
     echo --------------添加TAG----------------
-    docker tag hicoin-web-app 192.168.31.245:5000/hicoin-web-$var:v1.${BUILD_NUMBER}
+    docker tag hicoin-web-$var 192.168.31.245:5000/hicoin-web-$var:v1.${BUILD_NUMBER}
     echo --------------推送到仓库----------------
     docker push 192.168.31.245:5000/hicoin-web-$var:v1.${BUILD_NUMBER}
     echo --------------删除本地镜像----------------
