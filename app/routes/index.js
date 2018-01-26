51cc3c803f73167bbce0dc58831ccbbd13b06124
dengyc <dengyc2013@gmail.com>
2018-01-26 18:42:34 +0800

    updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,12 +6,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  fetch("http://hicoin-web-app:3000/test").then(req=>{
-    return req.json()
-  }).then(json=>{
-    res.render('index', { title: 'Express:'+json.url,now:json.now });
-  })
+ 
+    res.render('index', { title: 'Express:' });
 
 });
+
+router.get("/test",function(req,res){
+  fetch("http://hicoin-web-app:3000/test").then(req=>{
+    return req.json()
+  }).then(json=>{    
+      res.json(json);
+  })
+})
 
 module.exports = router;
