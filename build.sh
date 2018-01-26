e8d3cf65f37b9d3d7588ee37a75213a932d97475
dengyc <dengyc2013@gmail.com>
2018-01-26 10:45:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
+registry="192.168.31.245:5000"
+
 declare -A dic
 dic=([app]="./app" [api]="./api")
-
 
 for key in $(echo ${!dic[*]})
 do
@@ -10,10 +11,10 @@ do
     echo --------------开始编译 $key----------------
     docker build -t hicoin-web-$key ${dic[$key]}
     echo --------------添加TAG----------------
-    docker tag hicoin-web-$key 192.168.31.245:5000/hicoin-web-$key:v1.${BUILD_NUMBER}
+    docker tag hicoin-web-$key $registry/hicoin-web-$key:v1.${BUILD_NUMBER}
     echo --------------推送到仓库----------------
-    docker push 192.168.31.245:5000/hicoin-web-$key:v1.${BUILD_NUMBER}
+    docker push $registry/hicoin-web-$key:v1.${BUILD_NUMBER}
     echo --------------删除本地镜像----------------
     docker rmi hicoin-web-$key
-    docker rmi 192.168.31.245:5000/hicoin-web-$key:v1.${BUILD_NUMBER}
+    docker rmi $registry/hicoin-web-$key:v1.${BUILD_NUMBER}
 done
