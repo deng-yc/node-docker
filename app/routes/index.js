207ab9e12491faab2bd3a14dd5f66f2ae7ad691b
dengyc <dengyc2013@gmail.com>
2018-02-01 13:05:25 +0800

    updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,8 +7,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var lastTitle="";
+    if(fs.exists("/data/1.txt")){
+      lastTitle=fs.readFileSync("/data/1.txt");
+    }
     fs.writeFileSync("/data/1.txt",os.hostname());
-    res.render('index', { title: os.hostname() });
+    res.render('index', { title: os.hostname(),last_title:lastTitle });
 });
 
 module.exports = router;
