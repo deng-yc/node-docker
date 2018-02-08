11bdfdaf939f3661436ca3e199db7edba3af3e6d
dengyc <dengyc2013@gmail.com>
2018-02-08 16:40:45 +0800

    updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,15 +5,30 @@ var path = require('path');
 var fs = require("fs");
 var router = express.Router();
 
+
+
+function getLocalIPv4(){
+
+  var os = require('os');
+    var interfaces = os.networkInterfaces();
+    var IPv4 = '127.0.0.1';
+    for (var key in interfaces) {
+      var alias = 0;
+      interfaces[key].forEach(function(details){
+        if (details.family == 'IPv4' && key == 'en0'  ) {
+            IPv4 = details.address;
+        }
+      });
+    }
+    return IPv4;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var lastTitle="";
     var os_name=os.hostname();
-    if(fs.existsSync("/data/1.txt")){
-      lastTitle=fs.readFileSync("/data/1.txt",'utf-8');
-    }     
-    fs.writeFileSync("/data/1.txt",os_name);
-    res.render('index', { title: os_name,last_title:lastTitle });
+    var ip=getLocalIPv4();
+    res.render('index', { title: os_name,last_title:ip });
 });
 
 module.exports = router;
--- build.sh
+++ build.sh
@@ -1,22 +1,33 @@
 #!/bin/bash 
 
-prefix="test"
+prefix="hicoin"
 declare -A dic
-dic=([app]="./app")
+dic=([test-app]="./app")
 #登录docker仓库,如果需要
 #docker login --username admin --password admin localhub.online
-for key in $(echo ${!dic[*]})
-do
-    echo "$key : ${dic[$key]}"
-    echo --------------开始编译 $key----------------
-    docker build -t $prefix-$key ${dic[$key]}
-    echo --------------添加TAG----------------
-    docker tag $prefix-$key $REGISTRY_API/$prefix-$key:v1.${BUILD_NUMBER}
-    echo --------------推送到仓库----------------
-    docker push $REGISTRY_API/$prefix-$key:v1.${BUILD_NUMBER}
-    echo --------------删除本地镜像----------------
-    docker rmi $prefix-$key
-    docker rmi $REGISTRY_API/$prefix-$key:v1.${BUILD_NUMBER}
-done
 
+#登录docker仓库,如果需要
+#docker login --username admin --password admin localhub.online
+for app in $(echo ${!apps[*]})
+do
+    echo "$app : ${apps[$app]}"
+
+    echo --------------开始编译 $app----------------
+
+    docker build -t $prefix:$app.build-${BUILD_NUMBER} -f ${apps[$app]}/Dockerfile ${apps[$app]}
+
+    echo --------------添加TAG----------------
+
+    docker tag $prefix:$app.build-${BUILD_NUMBER} $REGISTRY_API/$prefix:$app.build-${BUILD_NUMBER}
+
+    echo --------------推送到仓库----------------
+
+    docker push $REGISTRY_API/$prefix:$app.build-${BUILD_NUMBER}
+
+    echo --------------删除本地镜像----------------
+
+    docker rmi $prefix:$app.build-${BUILD_NUMBER}
+
+    docker rmi $REGISTRY_API/$prefix:$app.build-${BUILD_NUMBER}
+done
 
