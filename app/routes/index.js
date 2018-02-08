4bca6f116cc38986fb669bb5da72ce45a5df3a04
dengyc <dengyc2013@gmail.com>
2018-02-08 17:06:22 +0800

    updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,30 +5,15 @@ var path = require('path');
 var fs = require("fs");
 var router = express.Router();
 
+var ip = require("ip");
 
 
-function getLocalIPv4(){
-
-  var os = require('os');
-    var interfaces = os.networkInterfaces();
-    var IPv4 = '127.0.0.1';
-    for (var key in interfaces) {
-      var alias = 0;
-      interfaces[key].forEach(function(details){
-        if (details.family == 'IPv4' && key == 'en0'  ) {
-            IPv4 = details.address;
-        }
-      });
-    }
-    return IPv4;
-}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var lastTitle="";
     var os_name=os.hostname();
-    var ip=getLocalIPv4();
-    res.render('index', { title: os_name,last_title:ip });
+    res.render('index', { title: os_name,last_title:ip.address() });
 });
 
 module.exports = router;
